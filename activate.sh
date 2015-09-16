cfaed389a8f7e709a6fc23185b094fef1d540c09
Steve Mansour <sman@stevemansour.com>
2015-09-16 09:19:57 -0700

    added tgo activation script

---
+++ activate.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# activation script for tgo
+
+usage() {
+    cat << ZZEOF
+
+Usage:   activate.sh [OPTIONS] cmd
+cmd is one of: START | STOP | READY | TEST
+
+OPTIONS
+-p port number on which to listen
+
+Examples:
+Command to start application on port 8081:
+
+	bash$  activate.sh -p 8081 START 
+
+Command to stop the application on port 8081:
+
+	bash$  activate.sh -p 8081 STOP
+
+ZZEOF
+
+	exit 0
+}
+
+start() {
+	./tgo -d
+}
+
+while getopts ":p:ih:" o; do
+    case "${o}" in
+        h)
+            usage
+            ;;
+        p)
+            PORT=${OPTARG}
+	    echo "PORT set to: ${PORT}"
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+for arg do
+	# echo '--> '"\`$arg'"
+    case "$arg" in
+	"START")
+		echo "START tgo"
+		./tgo -d &
+        echo "Started tgo, process id = $!"
+		;;
+	"STOP")
+		echo "STOP"
+		;;
+	"READY")
+		echo "READY"
+		;;
+	"TEST")
+		echo "TEST"
+		;;
+	*)
+		echo "Unrecognized command: $arg"
+		exit 1
+		;;
+    esac
+done
