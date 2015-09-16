9728499d955fcc7ed2e9a26b526795f08c6d1b84
Steve Mansour <sman@stevemansour.com>
2015-09-16 15:37:45 -0700

    fixed data issue in state

--- state.go
+++ state.go
@@ -23,6 +23,25 @@ func dPrintStatusReply(r *StatusReply) {
 	}
 }
 
+func PostStatusAndGetReply(state string, r *StatusReply) {
+	s := StatusMsg{state, "TGOtest", "tgo0", time.Now().Format(time.RFC822)}
+	rc, e := PostStatus(&s, r)
+	if nil != e {
+		ulog("PostStatus returned error:  %v\n", e)
+		os.Exit(5)
+	}
+
+	if rc != 200 {
+		ulog("Bad HTTP response code: %d\n", rc)
+	}
+
+	if r.ReplyCode != RespOK {
+		ulog("Uhura is not happy:  response to status: %d\n", r.ReplyCode)
+		dPrintStatusReply(r)
+		os.Exit(1)
+	}
+}
+
 func StateInit() {
 
 }
@@ -40,24 +59,7 @@ func StateDone() {
 }
 
 func InitiateStateMachine() {
-	var s = StatusMsg{"INIT", "TGOtest", "gto0", ""}
-	s.Tstamp = time.Now().Format(time.RFC822)
-
 	var r StatusReply
-	rc, e := PostStatus(&s, &r)
-	if nil != e {
-		ulog("PostStatus returned error:  %v\n", e)
-		os.Exit(5)
-	}
-
-	if rc != 200 {
-		ulog("Bad HTTP response code: %d\n", rc)
-	}
-
-	if r.ReplyCode != RespOK {
-		ulog("Uhura is not happy:  response to status: %d\n", r.ReplyCode)
-		dPrintStatusReply(&r)
-		os.Exit(1)
-	}
+	PostStatusAndGetReply("INIT", &r)
 	fmt.Printf("WE SUCCESSFULLY CONTACTED UHURA AND GOT A REPLY\n")
 }
